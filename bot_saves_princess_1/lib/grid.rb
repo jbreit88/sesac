ae6f07a7314b0400d81139bbfe7a42467e0caf83
Brad Breiten <jonathan@breiten.us>
2022-04-28 14:55:03 -0600

    Add methods to locate index location of bot and princess on board

--- bot_saves_princess_1/lib/grid.rb
+++ bot_saves_princess_1/lib/grid.rb
@@ -1,3 +1,6 @@
+require 'enumerator'
+require 'matrix'
+
 class Grid
 
   attr_reader :size,
@@ -6,6 +9,21 @@ class Grid
   def initialize(size, input)
     @size   = size
     @board  = input
+    @matrix = to_matrix
   end
 
+  def to_matrix
+    nested_array = []
+    @board.gsub(/\n/, '').chars.each_slice(@size) { |e| nested_array << e }
+
+    matrix = Matrix[*nested_array]
+  end
+
+  def locate_princess
+    @matrix.find_index('p')
+  end
+
+  def locate_bot
+    @matrix.find_index('m')
+  end
 end
